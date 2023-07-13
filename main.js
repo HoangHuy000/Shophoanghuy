b11e6bd83840cbce9ac1be205cdd3c794b7052e5
HoàngHuy <138868537+HoangHuy000@users.noreply.github.com>
2023-07-13 12:11:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-alert("ADMIN SẼ CẬP NHẬT ACC LIÊN TỤC MỖI NGÀY
-      VÀ HIỆN TẠI CHƯA CÓ ACC FREE FIRE") 
+alert("ADMIN SẼ CẬP NHẬT ACC LIÊN TỤC MỖI NGÀY VÀ HIỆN TẠI CHƯA CÓ ACC FREE FIRE") 
     
 setTimeout /* setInterval */ (function(){
     alert(" WEB CHỈ HOẠT ĐỘNG TỪ T2 - T6  ")
