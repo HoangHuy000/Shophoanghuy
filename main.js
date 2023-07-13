4bf1eb5646d8943644a5f131f757dd35687ce980
HoàngHuy <138868537+HoangHuy000@users.noreply.github.com>
2023-07-13 10:49:31 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("ADMIN SẼ CẬP NHẬT ACC LIÊN TỤC MỖI NGÀY") 
+
+alert("HIỆN TẠI CHƯA CÓ ACC FREE FIRE ")
+
+setTimeout /* setInterval */ (function(){
+    alert(" WEB CHỈ HOẠT ĐỘNG TỪ T2 - T6  ")
+},2000)
+
+function nofree(){
+    alert("huy")
+}
+
+/*      */
