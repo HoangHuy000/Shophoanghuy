c1099f46675607603530313c830c9615b4582462
HoàngHuy <138868537+HoangHuy000@users.noreply.github.com>
2023-07-13 11:00:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 alert("ADMIN SẼ CẬP NHẬT ACC LIÊN TỤC MỖI NGÀY") 
 
-alert("HIỆN TẠI CHƯA CÓ ACC FREE FIRE ")
-
+setTimeout /* setInterval */ (function(){
+    alert("HIỆN TẠI CHƯA CÓ ACC FREE FIRE ")
+},1000)
+    
 setTimeout /* setInterval */ (function(){
     alert(" WEB CHỈ HOẠT ĐỘNG TỪ T2 - T6  ")
 },2000)
-
-function nofree(){
-    alert("huy")
-}
 
 /*      */
